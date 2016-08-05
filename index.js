adadff69745df99eee466d95c4fe6bb66563ce65
bminixhofer <bj.minixhofer@gmail.com>
2016-08-05 08:17:50 +0200

    changed export structure

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 'use strict';
 
 const denodeify = require('denodeify');
-module.exports = class Decb {
-  constructor(config) {
-    this.ignore = config.ignore;
-    this.use = !this.ignore && config.use;
-  }
-  use(module) {
+module.exports = function(module, config = {}) {
     module = Object.create(module);
     for (var key in module)
       if (typeof module[key] === 'function') {
-        if (this.ignore) {
-          if (this.ignore.indexOf(key) > 0) return;
+        if (config.ignore) {
+          if (config.ignore.indexOf(key) > 0) return;
         }
-        if (this.use) {
-          if (this.use.indexOf(key) === -1) return;
+        if (config.use) {
+          if (config.use.indexOf(key) === -1) return;
         }
         module[key] = denodeify(module[key]);
       }
