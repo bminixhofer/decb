07413f8a749d4a3c0e4673eb09a89ed8bcc6213c
bminixhofer <bj.minixhofer@gmail.com>
2016-08-05 08:48:02 +0200

    rewrote to use native promises

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-'use strict';
-var fs = Object.create(require('fs'))
-var Promise = require('promise')
+let fs = require('./lib/promifs');
 
-module.exports = exports = fs
-for (var key in fs)
-  if (!(typeof fs[key] != 'function'
-      || key.match(/Sync$/)
-      || key.match(/^[A-Z]/)
-      || key.match(/^create/) 
-      || key.match(/^(un)?watch/)
-      ))
-  add(key)
-
-function add(key) {
-  var original = fs[key]
-  if (key !== 'exists')
-    fs[key] = Promise.denodeify(original)
-  else
-    fs[key] = function() {
-      var args = [].slice.call(arguments)
-      return new Promise(function(resolve, reject) {
-        args.push(resolve)
-        original.apply(null, args)
-      })
-    }
-}
+fs.readFile('README.md', 'utf8').then(file => {
+  console.log(file);
+});
