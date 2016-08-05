19a8302082ee35104c8194294068b2a913ac0da6
bminixhofer <bj.minixhofer@gmail.com>
2016-08-05 08:07:37 +0200

    added option to ignore functions

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const denodeify = require('denodeify');
-module.exports = function(module) {
-  module = Object.create(module);
-  for (var key in module)
-    if (typeof module[key] === 'function')
-      module[key] = denodeify(module[key]);
-  return module;
+module.exports = class Decb {
+  constructor(config) {
+    this.ignore = config.ignore;
+    this.use = !this.ignore && config.use;
+  }
+  use(module) {
+    module = Object.create(module);
+    for (var key in module)
+      if (typeof module[key] === 'function') {
+        if (this.ignore) {
+          if (this.ignore.indexOf(key) > 0) return;
+        }
+        if (this.use) {
+          if (this.use.indexOf(key) === -1) return;
+        }
+        module[key] = denodeify(module[key]);
+      }
+    return module;
+  }
 };
