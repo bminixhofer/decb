5a64296594eafcfe9078433066721a6c8c520308
bminixhofer <bj.minixhofer@gmail.com>
2016-08-05 08:45:51 +0200

    fixed untracked files

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-let fs = require('./lib/promifs');
+'use strict';
 
-fs.readFile('README.md', 'utf8').then(file => {
-  console.log(file);
-});
+const denodeify = require('denodeify');
+module.exports = function(module) {
+  module = Object.create(module);
+  for (var key in module)
+    if (typeof module[key] === 'function')
+      module[key] = denodeify(module[key]);
+  return module;
+};
