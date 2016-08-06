57f8612141e3cdf99d6d86f724698c017bbbc454
bminixhofer <bj.minixhofer@gmail.com>
2016-08-06 12:33:21 +0200

    rewrote main function

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const denodeify = require('denodeify');
-module.exports = function(module, config = {}) {
-    module = Object.create(module);
-    for (var key in module)
-      if (typeof module[key] === 'function') {
-        if (config.ignore) {
-          if (config.ignore.indexOf(key) > 0) return;
-        }
-        if (config.use) {
-          if (config.use.indexOf(key) === -1) return;
-        }
-        module[key] = denodeify(module[key]);
-      }
-    return module;
+module.exports = function(mod, config = {}) {
+  let keys = config.use || Object.keys(mod);
+  if(config.ignore) {
+    keys = keys.filter(key => {
+      return config.ignore.indexOf(key) === -1;
+    });
   }
+  console.log(keys);
+  keys.forEach(key => {
+    if (typeof mod[key] === 'function') {
+      mod[key] = denodeify(mod[key]);
+    }
+  });
+  return mod;
 };
